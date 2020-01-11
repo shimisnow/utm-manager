39c755b69848e265fce13ea5fa8239ef1acac604
Wadson Ferreira <wadson.ferreira@outlook.com>
2020-01-11 17:02:56 -0300

    Added tasks to generate a zip file with the minified code and a sha1 for the generate zip.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-const gulp   = require( 'gulp' );
-const gutil  = require( 'gulp-util' );
-const uglify = require( 'gulp-uglify' );
-const rm     = require( 'gulp-rm' );
-const rename = require( 'gulp-rename' );
+const gulp    = require( 'gulp' );
+const gutil   = require( 'gulp-util' );
+const uglify  = require( 'gulp-uglify' );
+const rm      = require( 'gulp-rm' );
+const rename  = require( 'gulp-rename' );
+const zip     = require( 'gulp-zip' );
+const hashsum = require( 'gulp-hashsum' );
 
 var paths = {
 	js: {
 		src: 'src/UTMManager.js',
 		dest: 'dist/',
 		filename: 'utm-manager.min.js'
+	},
+	zip : {
+		src: 'dist/**/*',
+		dest: 'dist/',
+		filename: 'utm-manager.zip'
+	},
+	hashsum : {
+		src: 'dist/utm-manager.zip',
+		dest: 'dist/',
+		filename: 'utm-manager.zip.sha1'
 	}
 };
 
@@ -17,11 +29,31 @@ gulp.task( 'clean', function() {
 		.pipe( rm() );
 } );
 
-gulp.task( 'minify', function() {
+gulp.task( 'compile', function() {
 	return gulp.src( paths.js.src )
 		.pipe( gutil.env.env === 'production' ? uglify() : gutil.noop() )
 		.pipe( rename( paths.js.filename ) )
 		.pipe( gulp.dest( paths.js.dest ) );
 } );
 
-gulp.task( 'default', gulp.series( [ 'clean', 'minify' ] ) );
+gulp.task( 'zip', function() {
+	return gulp.src( paths.zip.src )
+		.pipe( zip( paths.zip.filename ) )
+		.pipe( gulp.dest( paths.zip.dest ) );
+} );
+
+gulp.task( 'hashsum', function() {
+	return gulp.src( [ paths.hashsum.src ] )
+	.pipe( hashsum( {
+		dest : paths.hashsum.dest,
+		filename : paths.hashsum.filename
+	} ) );
+} );
+
+gulp.task( 'default',
+	gulp.series( [
+		'clean',
+		'compile',
+		gutil.env.env === 'production' ? [ 'zip', 'hashsum' ] : []
+	] )
+);
